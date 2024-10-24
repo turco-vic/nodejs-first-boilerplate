b41bcf3c550ff26cad15e140fb584fd2bc657fc2
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2024-10-24 11:38:43 -0300

    feat: create get all users

---
+++ src/routes/usuarios.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const usuariosRoutes = Router();
+
+let usuarios = [];
+
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message: 
+        usuarios.length == 0 
+        ? "Não há usuários cadastrados": 'Total de usuários: $(usuarios.lengh)', usuarios,
+    });
+});
+
+export default usuariosRoutes;
